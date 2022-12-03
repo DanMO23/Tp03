cdb6055c38d6045394a0c4b9cd5f1b9a7403bfc6
Guilherme Broedel Zorzal <guilhermezorzal2004@gmail.com>
2022-12-03 17:28:03 -0300

    Adicionado pastas e files

---
+++ Zorzal/funcoes_zorzal.c
@@ -0,0 +1,6 @@
+#include "funcoes_zorzal.h"
+
+int selecao(Tlista lista);
+int insercao(Tlista lista);
+int quicksort(Tlista lista);
+int heapsort(Tlista lista);
---
+++ Zorzal/funcoes_zorzal.h
@@ -0,0 +1,9 @@
+#include "../arquivos H/TADPalavra.h"
+#include "../arquivos H/TAD_lista.h"
+
+/* Implementar seleção, inserção, quicksort, heapsort*/
+
+int selecao(Tlista lista);
+int insercao(Tlista lista);
+int quicksort(Tlista lista);
+int heapsort(Tlista lista);
